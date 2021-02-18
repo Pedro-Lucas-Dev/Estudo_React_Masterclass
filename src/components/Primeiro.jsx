6c55fe61d184fea2c899e89aadfec9817b05ad4d
Pedro Lucas <Pedrolucas969210913@mgail.com>
2021-02-18 01:04:38 -0300

    MasterClass_React

---
+++ src/components/Primeiro.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+export default () => 
+        <>
+
+            <h1> Primeiro Componente  </h1>
+
+            <h2> Exemplo de um componente React </h2>
+        
+        </>
+
