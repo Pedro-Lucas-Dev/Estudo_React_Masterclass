9a7b9d248a1e0bec00c60251e20b095c521e54a4
Pedro Lucas <Pedrolucas969210913@mgail.com>
2021-02-20 23:10:16 -0300

    paramentros/props/treino

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,57 @@
 import './App.css'
 import React from 'react'
-import Primeiro from './components/Primeiro'
-import ComParamentro from './components/ComParamentro'
-import ComFilhos from './components/ComFilhos'
+import Primeiro from './components/basicos/Primeiro'
+import ComParamentro from './components/basicos/ComParamentro'
+import ComFilhos from './components/basicos/ComFilhos'
 import Card from './components/layout/Card'
-import Repeticao from './components/Repeticao'
-import Condicional from './components/Condicional'
-import CondicionalComIf from './components/CondicionalComIf'
+import Repeticao from './components/basicos/Repeticao'
+import Condicional from './components/basicos/Condicional'
+import CondicionalComIf from './components/basicos/CondicionalComIf'
+import Pai from './components/comunicacao/direta/Pai.jsx'
+import Super from './components/comunicacao/indireta/Super'
 
 
-
-export default props =>
+export default props => 
     <div className="App">
-        <Card titulo=" #06 - Condicional usando if ">
-            <CondicionalComIf numero={10}/>
+        <Card 
+            titulo=" #08 - Comunicação indireta "
+            color="#60047A"
+        >
+            <Super />
         </Card>
 
-        <Card titulo=" #05 - Condicional ">
+        <Card
+            titulo=" #07 - Comunicação direta" 
+            color=" #59323C"
+        >
+            <Pai sobrenome=" Freitas"/>
+        </Card>
+
+        <Card 
+            titulo=" #06 - Condicional usando if " 
+            color="#69D2E7"
+        >
+            <CondicionalComIf numero={10}/>      
+        </Card>
+
+        <Card 
+            titulo=" #05 - Condicional "
+            color="#E8B71A"
+        >
             <Condicional numero={11}/>
         </Card>
 
-        <Card titulo=" #04 - Repetição ">
+        <Card 
+            titulo=" #04 - Repetição "
+            color="#1FDA9A"
+        >
             <Repeticao />
         </Card>
 
-        <Card titulo=" #03 - Componente Com Filhos ">
-            
+        <Card 
+            titulo=" #03 - Componente Com Filhos "
+            color=" #5BB12F"
+        >
             <ComFilhos>
                 <ul>
 
@@ -38,21 +64,20 @@ export default props =>
             </ComFilhos>
         </Card>
 
-        <Card titulo=" #02 - Componente Com Paramentro ">
+        <Card 
+            titulo=" #02 - Componente Com Paramentro "
+            color="#982395"
+        >
             <ComParamentro 
                 titulo=" Esse é o título "
                 subtitulo=" Esse é o subtítulo "
             />
         </Card>
 
-        <Card titulo=" #01 - Primeiro Componente ">
+        <Card 
+            titulo=" #01 - Primeiro Componente "
+            color="#260126"
+        >
             <Primeiro />
         </Card>
-  
-        {/*
-        <ComParamentro 
-            titulo="Opa"
-            subtitulo="Epa"
-        /> */}
-
     </div>
---
+++ src/components/basicos/ComFilhos.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default props =>
+    <div>
+
+        <h2>Os Filhos:</h2>
+
+        <div>
+
+            { props.children }
+            
+        </div>
+
+    </div>
